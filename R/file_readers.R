6baeb0ae3fdb6b5b24f55aa9b68e8e67b1f81c52
Jannik Buhr <jannik.m.buhr@gmail.com>
2020-06-30 15:26:01 +0200

    initial commit

---
+++ R/file_readers.R
@@ -0,0 +1,19 @@
+#' Read xvg file
+#'
+#' And cleanup and parse data
+#'
+#' @param str :: filepath to the xvg file
+#'
+#' @return cleaned data  :: tibble
+#' @export
+read_xvg <- function(str) {
+  Peptides::readXVG(str) %>%
+    tidyr::pivot_longer(
+      cols = c(-Time),
+      names_pattern = "r_(\\d+) (.)",
+      names_to = c("r_id", ".value")
+    ) %>%
+    tidyr::rename_with(tolower) %>%
+    dplyr::mutate(across(.fns = parse_number))
+}
+
